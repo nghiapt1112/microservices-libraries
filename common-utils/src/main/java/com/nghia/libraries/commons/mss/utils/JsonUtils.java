9fd6bcdf686690b73c48f9315cf7225680d7f42d
nghia.pham <nghiapt@vnext.com.vn>
2019-06-05 17:24:41 +0700

    Update JsonUtils

--- common-utils/src/main/java/com/nghia/libraries/commons/mss/utils/JsonUtils.java
+++ common-utils/src/main/java/com/nghia/libraries/commons/mss/utils/JsonUtils.java
@@ -4,12 +4,17 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.*;
-import com.nghia.libraries.commons.mss.infrustructure.base.JsonSerializationException;
-import com.nghia.libraries.commons.mss.infrustructure.exception.DomainException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class JsonUtils {
     public static final ObjectMapper OBJECT_MAPPER;
@@ -19,7 +24,7 @@ public class JsonUtils {
 
         OBJECT_MAPPER
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, true)
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL)
                 .getSerializerProvider()
                 .setNullKeySerializer(new NullKeySerialize());
@@ -36,7 +41,7 @@ public class JsonUtils {
         try {
             return OBJECT_MAPPER.writeValueAsString(object);
         } catch (JsonProcessingException e) {
-            throw new DomainException(DomainException.Domain.General.getDomainCode(), e);
+            throw new JsonSerializationException("An error occur when parse json.", e);
         }
     }
 
@@ -44,23 +49,23 @@ public class JsonUtils {
         try {
             return OBJECT_MAPPER.<T>readValue(json, type);
         } catch (Exception e) {
-            throw new DomainException(DomainException.Domain.General.getDomainCode(), e);
+            throw new JsonSerializationException("An error occur when parse json.", e);
+        }
+    }
+
+    public static <T> Map toMap(T object) {
+        try {
+            return OBJECT_MAPPER.convertValue(object, LinkedHashMap.class);
+        } catch (IllegalArgumentException e) {
+            throw new JsonSerializationException("An error occur when parse json.", e);
         }
     }
 
     public static <T> byte[] toBytes(T object) {
         try {
             return OBJECT_MAPPER.writeValueAsBytes(object);
-        } catch (JsonProcessingException e) {
-            throw new DomainException(DomainException.Domain.General.getDomainCode(), e);
-        }
-    }
-
-    public static <T> T fromByte(byte[] json, Class<T> type) {
-        try {
-            return OBJECT_MAPPER.<T>readValue(json, type);
         } catch (Exception e) {
-            throw new DomainException(DomainException.Domain.General.getDomainCode(), e);
+            throw new JsonSerializationException("An error occur when parse json.", e);
         }
     }
 
@@ -82,7 +87,15 @@ public class JsonUtils {
 
 class NullKeySerialize extends JsonSerializer<Object> {
     @Override
-    public void serialize(Object value, JsonGenerator gen, SerializerProvider serializers) throws IOException, JsonProcessingException {
+    public void serialize(Object value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
         gen.writeFieldName("null");
     }
+}
+
+class JsonSerializationException extends RuntimeException {
+
+    public JsonSerializationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
